8b6f34a052407d2dfdcceb6f4b0f910b2fa5f816
Someone <me@example.com>
2021-09-27 14:18:22 +0300

    Test

---
+++ src/ru/stealthebat/Main.java
@@ -0,0 +1,13 @@
+package ru.stealthebat;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5, b = 4;
+
+        System.out.println("Hello, world!" + a + b);
+
+
+    }
+}
