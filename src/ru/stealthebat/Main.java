d34ae3e757390cc26e7d38fcd2920168907b4e2f
Someone <me@example.com>
2021-09-27 14:29:28 +0300

    More lines

--- src/ru/stealthebat/Main.java
+++ src/ru/stealthebat/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         int a = 5, b = 4;
-
+        int c = a + b;
+        boolean s1 = true;
         System.out.println("Hello, world!" + a + b);
 
-        System.out.println(a + b);
+        System.out.println(c);
 
         // new
     }
